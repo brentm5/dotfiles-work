b545257acc97f50b71fe371bd78c3fc0c2677154
Montague, Brent <bmontague@cvent.com>
2021-07-24 14:48:33 -0400

    New Changes

---
+++ zsh/configs/post/maven.zsh
@@ -0,0 +1,1 @@
+export MAVEN_OPTS="-Dmaven.artifact.threads=12 -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
--- zsh/configs/pre/path.zsh
+++ zsh/configs/pre/path.zsh
@@ -10,7 +10,7 @@ PATH="/opt/chefdk/bin:$PATH"
 # Make sure inspec is before chefdk
 PATH="/opt/inspec/bin:$PATH"
 
-# Make sure inspec is before chefdk
-PATH="$HOME/.toolbox-bash:$PATH"
+# Load intellij scripts
+PATH="$HOME/.toolbox:$PATH"
 
 export -U PATH
